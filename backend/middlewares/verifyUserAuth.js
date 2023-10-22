8a8fe7219113369080021d7adddd988623443329
ismail el youssefi <elyoussefiismail18@gmail.com>
2023-10-22 17:51:58 +0100

    Added verify user auth & User role midlewares

---
+++ backend/middlewares/verifyUserAuth.js
@@ -0,0 +1,4 @@
+  //TODO Implement user authentication logic here
+  //TODO For simplicity, this code assumes user authentication is already handled.
+  //TODO Set req.user with user details after successful authentication.
+  //TODO Decode jwt token and compare id to DB Id
