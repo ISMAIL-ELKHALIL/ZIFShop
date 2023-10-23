454ce45c5cc4940c3dca1e49bc68b87da224f416
ismail el youssefi <elyoussefiismail18@gmail.com>
2023-10-23 21:09:26 +0100

    added verifyUserAndRole and modified userController

--- backend/middlewares/verifyUserAuth.js
+++
@@ -1,4 +0,0 @@
-  //TODO Implement user authentication logic here
-  //TODO For simplicity, this code assumes user authentication is already handled.
-  //TODO Set req.user with user details after successful authentication.
-  //TODO Decode jwt token and compare id to DB Id
